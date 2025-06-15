c6f16aa636be30ae488fdc6c6a15b6f6a9f80e25
taufik69 <taufik35-2915@diu.edu.bd>
2025-06-16 00:49:04 +0600

    streaming

--- main.js
+++ main.js
@@ -1,108 +1,57 @@
-// // ১. events মডিউল ইমপোর্ট
-// const EventEmitter = require("events");
-
-// // ২. EventEmitter অবজেক্ট তৈরি
-// const myEmitter = new EventEmitter();
-
-// // ৩. ইভেন্টের জন্য লিসেনার সেট
-// myEmitter.on("greet", (name) => {
-//   console.log(`Hello, ${name}!`);
-// });
-
-// // ৪. ইভেন্ট ট্রিগার (emit)
-// myEmitter.emit("greet", "Taufik");
-
-// const event = require("events");
-// const eventEmiter = new event();
-
-// // now define a lishener for hearing the event
-// eventEmiter.on("hi", (prop) => {
-//   console.log("hi men how are you", prop);
-// });
-
-// eventEmiter.emit("hi", { name: "prop" });
-
-const { log } = require("console");
-
 const http = require("http");
-const event = require("events");
 const fs = require("fs");
 const path = require("path");
-const { ChildProcess } = require("child_process");
-const { json } = require("stream/consumers");
-const eventEmiter = new event();
+const { log } = require("console");
 
-eventEmiter.on("fire", async (callback) => {
-  const response = await fetch("https://dummyjson.com/products");
-  const data = await response.json();
-  callback(data);
-});
+const port = 4000;
 
 const server = http.createServer((req, res) => {
-  const pathname = path.join(__dirname, "data.txt");
+  if (req.url == "/") {
+    // serve the static file
+    const htmlPath = path.join(__dirname, "public", "index.html");
+    // fs.createReadStream(htmlPath).pipe(res);
 
-  // if (fs.existsSync(pathname)) {
-  //   res.end("already file Available");
-  // }
-  // // write data
-  // eventEmiter.emit("fire", (data) => {
-  //   fs.writeFile("data.txt", JSON.stringify(data), (err) => {
-  //     if (err) {
-  //       console.log("error from ", err);
-  //     } else {
-  //       console.log("file created Done");
-  //     }
-  //   });
-  // });
-
-  // routes
-  const routes = req.url.replace("/", "").toLocaleLowerCase();
-  if (routes == "getdata".toLocaleLowerCase()) {
-    //  grab a data then send it
-    // fs.readFile(pathname, "utf-8", (err, data) => {
-    //   if (err) {
-    //     log(err);
-    //   } else {
-    //     log("data is :", data);
-    //     res.end("done " + data);
-    //   }
-    // });
-
-    // usigng chunk or straming
-
+    const readStream = fs.createReadStream(htmlPath);
     let data = "";
-    const stream = fs.createReadStream(pathname);
-    const writeStream = fs.createWriteStream("xyz.txt");
+    readStream.on("data", (chunk) => {
+      data += chunk;
+    });
 
-    stream.pipe(writeStream);
-    // stream.on("data", (chunk) => {
-    //   writeStream.write(chunk);
-    //   data += chunk.toString();
-    // });
-    stream.on("end", () => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
+    // when end straming
+    readStream.on("end", () => {
+      res.writeHead(200, { "content-type": "text/html" });
       res.end(data);
     });
-    stream.on("error", (err) => {
-      res.writeHead(500);
-      res.end("File read error");
+  } else if (req.url == "/vedio") {
+    const vedioPath = path.join(__dirname, "vedios", "dash3.mov");
+    const vedioStatus = fs.statSync(vedioPath);
+    const fileSize = vedioStatus.size;
+    const range = req.headers.range;
+    if (!range) {
+      return res.end("Range header required for video streaming");
+    }
+    const parts = range.replace("bytes=", "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunkSize = end - start + 1;
+    //  now streaming
+    const stream = fs.createReadStream(vedioPath, { start, end });
+    // time duration ta slowly slowly incrase hobe
+    res.writeHead(206, {
+      "content-type": "vedio/mp4",
+      "content-range": `bytes ${start}-${end}/${fileSize}`,
+      "accept-ranges": "bytes",
+      "content-length": chunkSize,
     });
 
-    //
-  }
-  if (routes == "writeData".toLocaleLowerCase() && req.method == "POST") {
-    const writeStream = fs.createWriteStream("output.txt");
-    req.on("data", (chunk) => {
-      writeStream.write(chunk);
-    });
+    // stream.on("data", (chunk) => {
+    //   res.end(chunk);
+    // });
 
-    req.on("end", () => {
-      writeStream.end();
-      res.end("File write done");
-    });
+    stream.pipe(res);
   }
 });
 
-server.listen(4000, () => {
-  log("server Runnign on http://localhost:4000");
+server.listen(port, () => {
+  log(`http://localhost:${port}`);
 });
